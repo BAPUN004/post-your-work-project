b73fbb6dc5ecc7341de279b6fa5b48e306cd11b0
BAPUN004 <rnmohapatra46@gmail.com>
2026-04-19 18:51:55 +0530

    refactor: add comment to improve readability

--- bikeshare.py
+++ bikeshare.py
@@ -21,6 +21,7 @@ def get_filters():
 
     # get user input for month (all, january, february, ... , june)
 
+    # Refactor: improved readability
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
 
